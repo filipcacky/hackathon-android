690f9f383a3d89046e34354eb8ff09df8ab66c55
Filip Čacký <cacky.filip@gmail.com>
2018-11-23 12:18:29 +0100

    WebService WIP

--- WifiPi.Mobile/WifiPi.Mobile/Backend/WebService/WebService.cs
+++ WifiPi.Mobile/WifiPi.Mobile/Backend/WebService/WebService.cs
@@ -8,21 +8,49 @@ namespace WifiPi.Mobile.Backend.WebService
 		private HttpClient client;
 		private readonly string Url = "https://8b20e2c2.NGROK.io/";
 
-
-
-		public WebRepository()
+		public static class Paths
 		{
-			
+			#region General data
+			/// <summary>
+			/// api return general information on all devices
+			/// </summary>
+			public static string All => "all/";
+
+			#endregion
+
+			#region Specific device data
+			/// <summary>
+			/// used to create a path to specific device
+			/// </summary>
+			/// <param name="guid"></param>
+			/// <returns></returns>
+			public static string BasePath(string guid)
+			{
+				return $"device/{guid}/";
+			}
+			/// <summary>
+			/// Returns device specific array that contains statistics for last week
+			/// </summary>
+			public static string Week => "week/";
+			/// <summary>
+			/// Returns device specific array that contains statistics for last month
+			/// </summary>
+			public static string Month => "month/";
+			/// <summary>
+			/// Returns device specific array that contains statistics for this day
+			/// </summary>
+			public static string Today => "today/";
+			#endregion
 		}
 
-		public async Task<byte[]> GetFileFromUrl(string url, int timeout = 5000)
+		public async Task<byte[]> GetFileFromUrl(string path)
 		{
 			byte[] output = null;
 			try
 			{
 				using (this.client = new HttpClient())
 				{
-					var httpResponse = await this.client.GetAsync(this.Url + url);
+					var httpResponse = await this.client.GetAsync(this.Url + path);
 					output = await httpResponse.Content?.ReadAsByteArrayAsync();
 					return output;
 				}
---
+++ WifiPi.Mobile/WifiPi.Mobile/Models/SubModels/Gps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WifiPi.Mobile.Models.SubModels
+{
+	public class Gps
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+	}
+}
