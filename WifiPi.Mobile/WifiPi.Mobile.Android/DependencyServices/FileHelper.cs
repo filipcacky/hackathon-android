964b11fc4d46ea9e6bd51fb8ec3d7428f0ec9a0e
Ilja Larionov <larionov.ilja@gmail.com>
2018-11-23 11:57:49 +0100

    filehelper

---
+++ WifiPi.Mobile/WifiPi.Mobile.Android/DependencyServices/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using WifiPi.Mobile.DependencyServices;
+using WifiPi.Mobile.Droid.DependencyServices;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(FileHelper))]
+namespace WifiPi.Mobile.Droid.DependencyServices
+{
+	public class FileHelper : IFileHelper
+	{
+		public string GetAbsolutePath()
+		{
+			return Forms.Context.FilesDir.AbsolutePath + "/";
+		}
+	}
+}
